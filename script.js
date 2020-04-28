c80bcd8ead95bb4eca3d477a8964caba31795170
anleshch <lishchynskayaanna@gmail.com>
2020-04-28 11:13:01 +0300

    added new to JS

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-import {MDCFormField} from '@material/form-field';
-import {MDCCheckbox} from '@material/checkbox';
-import {MDCSwitch} from '@material/switch';
 
+import { MDCFormField } from '@material/form-field';
+import { MDCCheckbox } from '@material/checkbox';
+import { MDCSwitch } from '@material/switch';
+import { MDCRadio } from '@material/radio';
+
+const radio = new MDCRadio(document.querySelector('.md-radio'));
 const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
 const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
 formField.input = checkbox;
+formField.input = radio;
+
 const switchControl = new MDCSwitch(document.querySelector('.mdc-switch'));
